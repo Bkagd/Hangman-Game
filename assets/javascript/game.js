e00022022f15ee4a64c371a65494383772424591
Alan Drake <alangeorgedrake@gmail.com>
2017-11-11 01:10:18 -0700

    tentative

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 // Create an array of words
-    var teams = ["yankees", "cubs", "dodgers", "red sox", "indians", "blue jays", "astros", "mets", "angels", "cardinals", "giants", "orioles", "rangers", "royals", "brewers", "diamondbacks", "twins", "nationals", "tigers", "phillies", "rockies", "pirates", "mariners", "braves", "marlins", "white sox", "reds", "padres", "athletics"];
+    var teams = ["cubs", "red sox", "blue jays", "mets", "angels", "giants", "rangers", "royals", "twins", "tigers", "rockies", "pirates", "braves", "marlins", "white sox", "reds", "padres",];
 
 // Choose word randomly from array
     var randNum = Math.floor(Math.random() * teams.length);
     var chosenWord = teams[randNum];
-    var underScore = [];
     var rightWord = [];
     var wrongWord = [];
+    var underScore = [];
+
     console.log(chosenWord);
 
 // Create underscores based on length of chosen word
@@ -17,18 +18,36 @@
         return underScore;
     }
     console.log(generateUnderscore());
+// Display Underscores on page
+function placeScore() {
+    document.getElementsByClassName("underscore").innerHTML = "working";
+} 
+
 
 // Get the users guess
     document.addEventListener("keypress", (event) => {
         var keyCode = event.keyCode;
         var keyWord = String.fromCharCode(keyCode);
-        console.log(keyWord);
-    })
+// if guess is right
+        if (chosenWord.indexOf(keyWord) > -1) {
+            rightWord.push(keyWord);
+            underScore[chosenWord.indexOf(keyWord)] = keyWord;
+            console.log(underScore);
+            if (underScore.join("") == chosenWord) {
+                alert("You are the champ!");
+            }
+        } else {
+            wrongWord.push(keyWord);
+            console.log(wrongWord);
+            
+        }
+    });
+// Display underscores based on length of chosenWord
+   
+  
+
     
 
-// Correct Guess       
+     
      
     
-// Check to see if guess is correct
-// If right: push to right array
-// If wrong: push to wrong array
