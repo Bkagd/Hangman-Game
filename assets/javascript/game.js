70e9f6afba9e4b018b774aa54c382c358b8bac4b
Alan Drake <alangeorgedrake@gmail.com>
2017-11-14 19:26:44 -0700

    bit more cleaning up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 // Create an array of words
     var teams = ["cubs", "red sox", "blue jays", "mets", "angels", "giants", "rangers", "royals", "twins", "tigers", "rockies", "pirates", "braves", "marlins", "white sox", "reds", "padres",];
 
-// Choose word randomly from array
     var randNum = Math.floor(Math.random() * teams.length);
     var chosenWord = teams[randNum];
     var rightWord = [];
     var wrongWord = [];
     var underScore = [];
+    var win = 0;
+    var loss = 0;
 
     console.log(chosenWord);
 
@@ -33,22 +34,35 @@ function placeScore() {
 // if guess is right
         if (chosenWord.indexOf(keyWord) > -1) {
             rightWord.push(keyWord);
-            $(".rightGuess").text(rightWord.join(""));
+            $(".rightGuess").append(rightWord.join(""));
             underScore[chosenWord.indexOf(keyWord)] = keyWord;
             console.log(underScore);
             $(".underscore").text(underScore.join(" "));
-            if (underScore.join("") == chosenWord) {
+// if user Wins        
+        if (underScore.join("") == chosenWord) {
                 alert("You are the champ!");
+                win++
+                $(".win").text("Wins: " + win);
             }
         } else {
             wrongWord.push(keyWord);
-            $(".wrongGuess").text(wrongWord.join(""));
+            $(".wrongGuess").append(wrongWord.join(""));
             
             console.log(wrongWord);
             
         }
+
+        function reset() {
+            var randNum = Math.floor(Math.random() * teams.length);
+            var chosenWord = teams[randNum];
+            var rightWord = [];
+            var wrongWord = [];
+            var underScore = [];
+        }
+        $("#reset").click(reset());
     });
-// Display underscores based on length of chosenWord
+// Reset Button
+       
    
   
 
